f5a7fbc301b4cbeed24a71aad7540e86a56fee0f
hyeopsang <dbtkdguq3@gmail.com>
2025-08-26 20:27:17 +0900

    feat: 구글 맵 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {AdvancedMarker, APIProvider, Map} from '@vis.gl/react-google-maps';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const position = {lat: 53.54992, lng: 10.00678};
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <APIProvider apiKey={''}>
+        <Map defaultCenter={position} defaultZoom={10} mapId="DEMO_MAP_ID">
+          <AdvancedMarker position={position} />
+        </Map>
+      </APIProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
