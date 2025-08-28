3fc9fdd556dd34cf7766839adbd97e09a3ecb6b9
hyeopsang <dbtkdguq3@gmail.com>
2025-08-28 17:52:25 +0900

    fix: map instance ref => state

--- src/useGoogleMap.ts
+++ src/useGoogleMap.ts
@@ -1,27 +1,27 @@
-import {useEffect, useRef} from 'react';
-import {Loader} from '@googlemaps/js-api-loader';
+import { useEffect, useRef, useState } from "react";
+import { Loader } from "@googlemaps/js-api-loader";
 
 export const useGoogleMap = (
-  mapId = 'DEMO_MAP_ID',
-  defaultCenter = {lat: 37.5665, lng: 126.978}
+  mapId = "DEMO_MAP_ID",
+  defaultCenter = { lat: 37.5665, lng: 126.978 }
 ) => {
   const mapContainerRef = useRef<HTMLDivElement>(null);
-  const mapRef = useRef<google.maps.Map | null>(null);
+  const [mapInstance, setMapInstance] = useState<google.maps.Map | null>(null);
   const remoteMarkerRef = useRef<google.maps.Marker | null>(null);
 
   // 지도 초기화
   useEffect(() => {
     const loader = new Loader({
       apiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-      version: 'weekly',
+      version: "weekly",
     });
 
     loader.load().then(async () => {
-      const {Map} = (await google.maps.importLibrary(
-        'maps'
+      const { Map } = (await google.maps.importLibrary(
+        "maps"
       )) as google.maps.MapsLibrary;
-      const {AdvancedMarkerElement} = (await google.maps.importLibrary(
-        'marker'
+      const { AdvancedMarkerElement } = (await google.maps.importLibrary(
+        "marker"
       )) as google.maps.MarkerLibrary;
 
       const initMap = (center: google.maps.LatLngLiteral) => {
@@ -34,15 +34,15 @@ export const useGoogleMap = (
           disableDefaultUI: true,
         });
 
-        mapRef.current = map;
+        setMapInstance(map);
 
         const userMarker = new AdvancedMarkerElement({
           map,
           position: center,
-          title: '내 위치',
+          title: "내 위치",
         });
 
-        userMarker.addListener('gmp-click', () => {
+        userMarker.addListener("gmp-click", () => {
           map.setCenter(center);
         });
       };
@@ -67,19 +67,18 @@ export const useGoogleMap = (
 
   // 상대 위치 업데이트
   const updateRemoteMarker = (lat: number, lng: number) => {
-    if (!mapRef.current) return;
-
+    if (!mapInstance) return;
     if (!remoteMarkerRef.current) {
       remoteMarkerRef.current = new google.maps.Marker({
-        map: mapRef.current,
-        position: {lat, lng},
-        title: '상대 위치',
-        icon: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+        map: mapInstance,
+        position: { lat, lng },
+        title: "상대 위치",
+        icon: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png",
       });
     } else {
-      remoteMarkerRef.current.setPosition({lat, lng});
+      remoteMarkerRef.current.setPosition({ lat, lng });
     }
   };
 
-  return {mapContainerRef, updateRemoteMarker};
+  return { mapContainerRef, updateRemoteMarker };
 };
